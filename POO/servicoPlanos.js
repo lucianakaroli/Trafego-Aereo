2ecf302aa7f8745be792c47d0b39011ef02fa0a8
Luciana Silva <lucianakarolif@gmail.com>
2024-09-09 10:33:49 -0300

    testes

--- POO/servicoPlanos.js
+++ POO/servicoPlanos.js
@@ -3,41 +3,41 @@ const prompt = promptsync({sigint: true});
 import { validate, typedef } from "bycontract";
 import PlanoDeVoo from './planoDeVoo.js';
 
-class ServicoPlanos{
-    _planos;
+    class ServicoPlanos{
+        _planos;
 
-    constructor(planos){
-        typedef('PlanodeVoo', {
-            _id: "number",
-            _matricPiloto: "string",
-            _idAerovia: "string",
-            _data: "Date",
-            _horario: "string",
-            _altitude: "number",
-            _cancelado: "boolean"
-        });
+        constructor(planos){
+            typedef('PlanodeVoo', {
+                _id: "number",
+                _matricPiloto: "string",
+                _idAerovia: "string",
+                _data: "Date",
+                _horario: "string",
+                _altitude: "number",
+                _cancelado: "boolean"
+            });
 
-        validate(planos, "Array.<PlanodeVoo>");
-        this._planos = planos;
-    }
-
-
-    consiste(plano){
-    }
-
-    recupera(id){
-        if(!this._planos){
-            console.log(`Plano de voo com ID ${id} não encontrado.`);
-            return
+            validate(planos, "Array.<PlanodeVoo>");
+            this._planos = planos;
         }
 
-       return this._planos.find(planoDeVoo => planoDeVoo.getId() === id);
+
+        consiste(plano){
+        }
+
+        recupera(id){
+            if(!this._planos){
+                console.log(`Plano de voo com ID ${id} não encontrado.`);
+                return
+            }
+
+            return this._planos.find(planoDeVoo => planoDeVoo.getId() === id);
+        }t
+        
+        todos(){
+            return this._planos;
+        }
     }
-    
-    todos(){
-        return this._planos;
-    }
-}
 
 
 export default ServicoPlanos;
